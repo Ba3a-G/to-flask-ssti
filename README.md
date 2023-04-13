5218b1a2eedec5e899526b0a46e0c06dd80c0efd
Aryan Kumar <aryan.ak434@gmail.com>
2023-04-13 14:45:33 +0530

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# A simple Flask based CTF
+
+1. Installing the dependencies
+```python
+pip install Flask
+```
+2. Running the script
+```python
+python main.py
+```
+3. The webapp should be available at `localhost:port`.
---
+++ main.py
@@ -0,0 +1,12 @@
+# A Flask app with search bar
+from flask import Flask, render_template, request, render_template_string
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    if request.args.get('name'):
+        return render_template('info.html', name=request.args.get('name'))
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=False)
