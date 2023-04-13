1ee92e91867b4cbaf27cbf400cb7781bd0a9650f
Aryan Kumar <aryan.ak434@gmail.com>
2023-04-13 14:55:48 +0530

    successfully bugged 💪💪

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     if request.args.get('name'):
-        return render_template('info.html', name=request.args.get('name'))
+        payload = render_template_string(request.args.get('name'))
+        return render_template('info.html', name=payload)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
